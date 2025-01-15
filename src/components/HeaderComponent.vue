eef503346320c811c9e1386f2916925d8cb33b1b
Talibert <guilhermetaliberti@gmail.com>
2025-01-15 00:01:45 -0300

    Mexendo com diretivas

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,17 @@
 <template>
   <header class="header">
-    <HeaderButton label="Botão 1" />
-    <HeaderButton label="Botão 2" />
-    <HeaderButton label="Botão 3" />
-    <HeaderButton label="Botão 4" />
+    <div class="header-title">
+      Nome: {{ nome }}
+    </div>
+    <div class="header-buttons">
+      <HeaderButton label="Botão 1" 
+      v-show="showButton1" />
+      <HeaderButton label="Botão 2" 
+      v-if="showButton2" />
+      <HeaderButton label="Botão 3" />
+      <HeaderButton label="Botão 4" />
+    </div>
+    
   </header>
 </template>
 
@@ -16,16 +24,34 @@ export default {
   components: {
     HeaderButton,
   },
+  data(){
+    return {
+      showButton1: false,
+      showButton2: false,
+      nome: "vueTest"
+    }
+  }
 };
 
 </script>
 
 <style scoped>
-.header {
+.header{
   display: flex;
-  justify-content: space-around;
   align-items: center;
+  justify-content: space-between;
   background-color: #1aa72c;
-  padding: 20px;
+  padding: 0 20px;
+}
+
+.header-title{
+  margin-right: auto;
+  background-color: antiquewhite;
+}
+
+.header-buttons {
+  display: flex;;
+  padding: 15px;
+  gap: 30px;
 }
 </style>
